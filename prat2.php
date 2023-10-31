daae1308f3cedacce306adc4a30e5a3b6fbad185
pricillgabrell07 <149454779+pricillgabrell07@users.noreply.github.com>
2023-10-31 23:52:37 +0700

    prat02.php

--- prat2.php
+++ prat2.php
@@ -49,7 +49,7 @@
             </tr>
         </table>
         </div>
-        <a href="input.php"><I>Back To Home</I></a>
+        <a href="prat1.php"><I>Back To Home</I></a>
     </div>
     <?php ?>  
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>      
